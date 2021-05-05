c9da002be7d8bc41e118b5597f2b2ce8d5c20198
Andreas <andreas.lang@student.upt.ro>
2021-05-06 00:52:43 +0300

    Added new things

---
+++ src/main/java/controller/MainController.java
@@ -0,0 +1,16 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class MainController {
+    @FXML
+    private Button b_register;
+
+    @FXML Button b_login;
+
+    public void handler_register(ActionEvent action)
+    {
+    }
+}
