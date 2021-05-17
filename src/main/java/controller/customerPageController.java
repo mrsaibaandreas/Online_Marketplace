43170f6e3b7a354ff18d9e9d85d3c5e3587c6f7a
Lesll Armand <lesllarmand@yahoo.com>
2021-05-17 15:05:04 +0300

    OM-45
    Added OM-46 too

---
+++ src/main/java/controller/customerPageController.java
@@ -0,0 +1,74 @@
+package controller;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+import javax.swing.text.html.ImageView;
+import java.awt.event.ActionEvent;
+import java.io.IOException;
+
+public class customerPageController {
+
+    @FXML
+    private Pane pane;
+
+    @FXML
+    private ImageView imagerightcorner;
+
+    @FXML
+    private ImageView imageCenter;
+
+    @FXML
+    private Button registerButton;
+
+    @FXML
+    private Button loginButton;
+
+    @FXML
+    void loginButtonAction(ActionEvent event) {
+    }
+
+    @FXML
+    private Button goBackButton;
+
+    @FXML
+    void goBackButtonAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void registerButtonAction(ActionEvent event) {
+
+    }
+
+    public void registerButtonAction(javafx.event.ActionEvent actionEvent) {
+    }
+
+    public void loginButtonAction(javafx.event.ActionEvent actionEvent)  throws IOException {
+        Parent NewCustomer = FXMLLoader.load(getClass().getResource("/login.fxml"));
+        Scene NewCustomerScene = new Scene(NewCustomer);
+
+        //Here we get the stage information
+        Stage window=(Stage)((Node)actionEvent.getSource()).getScene().getWindow();
+
+        window.setScene(NewCustomerScene);
+        window.show();
+    }
+
+    public void goBackButtonAction(javafx.event.ActionEvent actionEvent) throws IOException {
+        Parent NewCustomer = FXMLLoader.load(getClass().getResource("/welcomePage.fxml"));
+        Scene NewCustomerScene = new Scene(NewCustomer);
+
+        //Here we get the stage information
+        Stage window=(Stage)((Node)actionEvent.getSource()).getScene().getWindow();
+
+        window.setScene(NewCustomerScene);
+        window.show();
+    }
+}
---
+++ src/main/java/controller/welcomePageController.java
@@ -0,0 +1,72 @@
+
+//        Parent NewCustomer = FXMLLoader.load(getClass().getResource("customerPage.fxml"));
+//        Scene NewCustomerScene = new Scene(NewCustomer);
+//
+//        //Here we get the stage information
+//        Stage window=(Stage)((Node)event.getSource()).getScene().getWindow();
+//
+//        window.setScene(NewCustomerScene);
+//        window.show();
+
+package controller;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+import java.awt.event.ActionEvent;
+import java.io.IOException;
+
+public class welcomePageController {
+
+    @FXML
+    private Text welcomeText;
+
+    @FXML
+    private ImageView welcomeImage;
+
+    @FXML
+    private Text questionText;
+
+    @FXML
+    private Button custommerButton;
+
+    @FXML
+    private Button supllierButton;
+
+    @FXML
+    void customerButtonAction(ActionEvent event){
+    }
+    @FXML
+    void supplierButtonAction(ActionEvent event){
+    }
+
+    public void customerButtonAction(javafx.event.ActionEvent actionEvent)  throws IOException {
+        Parent NewCustomer = FXMLLoader.load(getClass().getResource("/customerPage.fxml"));
+        Scene NewCustomerScene = new Scene(NewCustomer);
+
+        //Here we get the stage information
+        Stage window=(Stage)((Node)actionEvent.getSource()).getScene().getWindow();
+
+        window.setScene(NewCustomerScene);
+        window.show();
+    }
+
+    public void supplierButtonAction(javafx.event.ActionEvent actionEvent) throws IOException{
+        Parent NewCustomer = FXMLLoader.load(getClass().getResource("/supplierPage.fxml"));
+        Scene NewCustomerScene = new Scene(NewCustomer);
+
+        //Here we get the stage information
+        Stage window=(Stage)((Node)actionEvent.getSource()).getScene().getWindow();
+
+        window.setScene(NewCustomerScene);
+        window.show();
+    }
+}
+
