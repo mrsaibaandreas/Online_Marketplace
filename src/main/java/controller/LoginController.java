55b3add86a5f2f4dcc6e5bfe11836f63468ac3fe
Lesll Armand <lesllarmand@yahoo.com>
2021-05-17 15:26:39 +0300

    UM-45
    added a back button in login page

--- src/main/java/controller/LoginController.java
+++ src/main/java/controller/LoginController.java
@@ -1,4 +1,81 @@
 package controller;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
+
+import java.awt.event.ActionEvent;
+import java.io.IOException;
+
 public class LoginController {
+
+    @FXML
+    private PasswordField writePassword;
+
+    @FXML
+    private TextField writeUsername;
+
+    @FXML
+    private Label usernameText;
+
+    @FXML
+    private Label passwordText;
+
+    @FXML
+    private Button loginButton;
+
+    @FXML
+    private ImageView imageCenter;
+
+    @FXML
+    private Button backButton;
+
+    @FXML
+    void backButtonAction(ActionEvent event){
+
+    }
+
+    @FXML
+    void loginButtonAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void writePasswordAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void writeUsernameAction(ActionEvent event) {
+
+    }
+
+    public void backButtonAction(javafx.event.ActionEvent actionEvent) throws IOException{
+        Parent NewCustomer = FXMLLoader.load(getClass().getResource("/customerPage.fxml"));
+        Scene NewCustomerScene = new Scene(NewCustomer);
+
+        //Here we get the stage information
+        Stage window=(Stage)((Node)actionEvent.getSource()).getScene().getWindow();
+
+        window.setScene(NewCustomerScene);
+        window.show();
+    }
+
+    public void writePasswordAction(javafx.event.ActionEvent actionEvent) {
+    }
+
+    public void writeUsernameAction(javafx.event.ActionEvent actionEvent) {
+    }
+
+    public void loginButtonAction(javafx.event.ActionEvent actionEvent) {
+    }
 }
+
