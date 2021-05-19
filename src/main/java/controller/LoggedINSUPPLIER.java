15d6605cafc1ffea6688021800ab424678b81fda
Lesll Armand <lesllarmand@yahoo.com>
2021-05-19 10:45:06 +0300

    created separate login for supplier
    also created supplier interface after loggin in

---
+++ src/main/java/controller/LoggedINSUPPLIER.java
@@ -0,0 +1,48 @@
+package controller;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
+
+import java.awt.event.ActionEvent;
+import java.io.IOException;
+
+public class LoggedINSUPPLIER {
+
+        @FXML
+        private ImageView backgroundSUPPLIER;
+
+        @FXML
+        private Button viewItemsListedButton;
+
+        @FXML
+        private Button logOutButton;
+
+        @FXML
+        void logOutButtonAction(ActionEvent event) {
+
+        }
+
+        @FXML
+        void viewItemsListedButtonAction(ActionEvent event) {
+
+        }
+
+    public void viewItemsListedButtonAction(javafx.event.ActionEvent actionEvent) {
+    }
+
+    public void logOutButtonAction(javafx.event.ActionEvent actionEvent) throws IOException {
+        Parent NewCustomer = FXMLLoader.load(getClass().getResource("/welcomePage.fxml"));
+        Scene NewCustomerScene = new Scene(NewCustomer);
+
+        //Here we get the stage information
+        Stage window=(Stage)((Node)actionEvent.getSource()).getScene().getWindow();
+
+        window.setScene(NewCustomerScene);
+        window.show();
+    }
+}
